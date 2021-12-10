47b56dd5ce15e56bd5c84a168c17f08b2a5ffc59
Andreas Mewald <a.mewald@pke.at>
2021-12-10 11:25:38 +0100

    Added process runner for sut

--- src/cli.test/cli.test/End2End/ReadDummyData.cs
+++ src/cli.test/cli.test/End2End/ReadDummyData.cs
@@ -2,22 +2,17 @@
 using FluentAssertions;
 using Newtonsoft.Json;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace cli.test.End2End
 {
     [TestFixture]
-    public class ReadDummyData
+    public partial class ReadDummyData
     {
-        readonly StringWriter _writer = new();
-        private string _sutResult;
 
         [SetUp]
         public void Setup()
@@ -26,50 +21,10 @@ namespace cli.test.End2End
             File.WriteAllText(@"c:\todo.json", JsonConvert.SerializeObject(todo));
         }
 
-
         [Test]
         public void ShowTodos()
         {
-            RunSut(string.Empty);
-            _sutResult.Should().Be("1 Dummy false");
-        }
-
-
-        private void RunSut(string args)
-        {
-            // use ProcessStartInfo class.
-            ProcessStartInfo startInfo = new ProcessStartInfo();
-            startInfo.CreateNoWindow = false;
-            startInfo.UseShellExecute = false;
-
-            // Get path to exe
-            var pathToTestDll = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            var pathToExt = Path.Combine(pathToTestDll, @"..\..\..\..\..\cli\bin\Debug\net5.0\cli.exe");
-            startInfo.FileName = pathToExt;
-
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            startInfo.RedirectStandardOutput = true;
-
-            // set arguments.
-            startInfo.Arguments = args;
-
-            try
-            {
-                // start with the info we specified.
-                using Process process = Process.Start(startInfo);
-                // Redirect stdout to reader
-                using (StreamReader reader = process.StandardOutput)
-                {
-                    _sutResult = reader.ReadToEnd();
-                }
-
-                process.WaitForExit();
-            }
-            catch(Exception ex)
-            {
-                // Log error.
-                Assert.Fail(ex.ToString());
-            }
+            new SutRunner().RunSut(string.Empty).Should().Be("1 Dummy false");
         }
     }
 }
---
+++ src/cli.test/cli.test/End2End/SutRunner.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace cli.test.End2End
+{
+    public partial class ReadDummyData
+    {
+        public class SutRunner
+        {
+            public string RunSut(string args)
+            {
+                // use ProcessStartInfo class.
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.CreateNoWindow = false;
+                startInfo.UseShellExecute = false;
+
+                // Get path to exe
+                var pathToTestDll = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                var pathToExt = Path.Combine(pathToTestDll, @"..\..\..\..\..\cli\bin\Debug\net5.0\cli.exe");
+                startInfo.FileName = pathToExt;
+
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.RedirectStandardOutput = true;
+
+                // set arguments.
+                startInfo.Arguments = args;
+
+                var processStdout = string.Empty;
+                try
+                {
+                    // start with the info we specified.
+                    using Process process = Process.Start(startInfo);
+                    // Redirect stdout to reader
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        processStdout = reader.ReadToEnd();
+                    }
+
+                    process.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    // Log error.
+                    Assert.Fail(ex.ToString());
+                }
+
+                return processStdout;
+            }
+        }
+
+    }
+}
