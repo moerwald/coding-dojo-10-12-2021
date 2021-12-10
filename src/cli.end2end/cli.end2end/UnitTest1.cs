438960addc9691bb6db038458acb9a286b82043b
Andreas Mewald <a.mewald@pke.at>
2021-12-10 10:13:24 +0100

    initial commit

---
+++ src/cli.end2end/cli.end2end/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace cli.end2end
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
