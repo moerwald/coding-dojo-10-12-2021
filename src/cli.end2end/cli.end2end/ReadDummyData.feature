30f7388e0a7078424b8fff53eccc30893778e582
Andreas Mewald <a.mewald@pke.at>
2021-12-14 07:21:19 +0100

    Moved end2end unit tests to specflow project

---
+++ src/cli.end2end/cli.end2end/ReadDummyData.feature
@@ -0,0 +1,9 @@
+﻿Feature: ReadDummyData
+	We want to start the program with specific args and check the ouput dumped on stdout
+
+Scenario: Start the CLI program and check stdout for dummy entry
+	Given A file with testdata
+	When I start the CLI program with argument "ShowAll"
+	Then the following text shall be dropped on stdout
+		| Stdout                                        |
+		| 1 Dummy False~NewLine~2 Dummy2 False~NewLine~ |
---
+++ src/cli.end2end/cli.end2end/Steps.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using static cli.test.End2End.ReadDummyData;
+
+namespace cli.end2end
+{
+    [Binding]
+    public class Steps
+    {
+        private readonly ScenarioContext _context;
+        private static readonly string _stdout = "Stdout";
+
+        public Steps(ScenarioContext context) => _context = context;
+
+        [Given(@"A file with testdata")]
+        public static void GivenAFileWithTestdata() 
+            => 
+            File.WriteAllText(@"c:\todo.json", "[{\"Id\":1,\"Title\":\"Dummy\",\"IsCompleted\":false}, {\"Id\":2,\"Title\":\"Dummy2\",\"IsCompleted\":false}]");
+
+        [When(@"I start the CLI program with argument ""(.*)""")]
+        public void WhenIStartTheCLIProgramWithArgument(string args) 
+            => 
+            _context.Add(_stdout, SutRunner.Run(args));
+
+        [Then(@"the following text shall be dropped on stdout")]
+        public void ThenTheFollowingTextShallBeDroppedOnStdout(Table table) 
+            => 
+            table.Rows.ToList().ForEach(row => _context.Get<string>(_stdout)
+                                                       .Should()
+                                                       .Be(row[_stdout].Replace("~NewLine~",Environment.NewLine)));
+
+    }
+}
